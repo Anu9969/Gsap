2bfebca2f5d632fbef2aa96d101eb8eb16cce2ac
Anu9969 <sharmaanurag9969@gmail.com>
2025-09-16 23:07:04 +0530

    initial commit with setup

---
+++ gsap/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='text-blue-600 p-1 bg-amber-300'>Hello Gsap!</h1>
+    </div>
+  )
+}
+
+export default App
